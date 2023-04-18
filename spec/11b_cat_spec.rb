dc8cf234b2a93cb8de21f57dda468582be2562bf
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-04-18 15:45:52 +0100

    Complete 11b_cat_spec

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  let(:subject) {described_class.new("Whiskers")}
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'with Cat\'s own method' do
+    include_examples "shared method name"
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(subject).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(subject).to be_hiding
   end
 end
