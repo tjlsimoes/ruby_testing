7cad48db3463e1efe1002f1493cb457f45e42aec
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-04-20 11:55:14 +0100

    Complete 11c_dog_spec.rb

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:ernesto) { described_class.new("Max")}
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when Dog has method name shared with other classes' do
+    include_examples "shared method name"
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(ernesto).not_to be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(ernesto).to be_sleeping
   end
 end
